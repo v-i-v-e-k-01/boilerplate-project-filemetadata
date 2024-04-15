cb9ec3265946ecaeaa1f894a34ea3ae0fe261322
Vivek D <vivek01110110@gmail.com>
2024-04-15 15:12:11 +0000

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	new file:   upload.js 	new file:   uploads/1713193854934 - new 2.txt

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const upload = require("./upload"); // upload.js imported here, as upload.js has multer imported, no need to import here
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res,next)=>{
+  res.json({
+    name: req.file.fieldname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+  next();
+})
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
